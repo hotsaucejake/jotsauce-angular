0396a817dc158c78b937076818f8c04604ce35b9
hotsaucejake <get@hotsaucejake.com>
2021-01-31 10:51:21 -0500

    clean start

--- src/app/shared/vertical-header/vertical-navigation.component.ts
+++ src/app/shared/vertical-header/vertical-navigation.component.ts
@@ -1,15 +1,7 @@
 import { Component, AfterViewInit, EventEmitter, Output } from '@angular/core';
-import {
-  NgbModal,
-  ModalDismissReasons,
-  NgbPanelChangeEvent,
-  NgbCarouselConfig
-} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 import { TranslateService } from '@ngx-translate/core';
-import { AppNotification } from 'src/app/core/interfaces/app/app-notification.interface';
-import { MyMessage } from 'src/app/core/interfaces/app/my-message.interface';
-import { Language } from 'src/app/core/interfaces/app/language.interface';
 
 
 @Component({
@@ -20,112 +12,11 @@ export class VerticalNavigationComponent implements AfterViewInit {
   @Output() toggleSidebar = new EventEmitter<void>();
 
   public config: PerfectScrollbarConfigInterface = {};
-
   public showSearch = false;
-
-  // This is for Notifications
-  notifications: AppNotification[] = [
-    {
-      btn: 'btn-danger',
-      icon: 'ti-link',
-      title: 'Luanch Admin',
-      subject: 'Just see the my new admin!',
-      time: '9:30 AM'
-    },
-    {
-      btn: 'btn-success',
-      icon: 'ti-calendar',
-      title: 'Event today',
-      subject: 'Just a reminder that you have event',
-      time: '9:10 AM'
-    },
-    {
-      btn: 'btn-info',
-      icon: 'ti-settings',
-      title: 'Settings',
-      subject: 'You can customize this template as you want',
-      time: '9:08 AM'
-    },
-    {
-      btn: 'btn-primary',
-      icon: 'ti-user',
-      title: 'Pavan kumar',
-      subject: 'Just see the my admin!',
-      time: '9:00 AM'
-    }
-  ];
-
-  // This is for Mymessages
-  mymessages: MyMessage[] = [
-    {
-      useravatar: 'assets/images/users/1.jpg',
-      status: 'online',
-      from: 'Pavan kumar',
-      subject: 'Just see the my admin!',
-      time: '9:30 AM'
-    },
-    {
-      useravatar: 'assets/images/users/2.jpg',
-      status: 'busy',
-      from: 'Sonu Nigam',
-      subject: 'I have sung a song! See you at',
-      time: '9:10 AM'
-    },
-    {
-      useravatar: 'assets/images/users/2.jpg',
-      status: 'away',
-      from: 'Arijit Sinh',
-      subject: 'I am a singer!',
-      time: '9:08 AM'
-    },
-    {
-      useravatar: 'assets/images/users/4.jpg',
-      status: 'offline',
-      from: 'Pavan kumar',
-      subject: 'Just see the my admin!',
-      time: '9:00 AM'
-    }
-  ];
-
-  public selectedLanguage: Language = {
-    language: 'English',
-    code: 'en',
-    type: 'US',
-    icon: 'us'
-  };
-
-  public languages: Language[] = [{
-    language: 'English',
-    code: 'en',
-    type: 'US',
-    icon: 'us'
-  },
-                                  {
-    language: 'Español',
-    code: 'es',
-    icon: 'es'
-  },
-                                  {
-    language: 'Français',
-    code: 'fr',
-    icon: 'fr'
-  },
-                                  {
-    language: 'German',
-    code: 'de',
-    icon: 'de'
-  }];
-
-
 
   constructor(private modalService: NgbModal, private translate: TranslateService) {
     translate.setDefaultLang('en');
   }
 
   ngAfterViewInit(): void { }
-
-  changeLanguage(lang: Language): void {
-    this.translate.use(lang.code);
-    this.selectedLanguage = lang;
-  }
 }
