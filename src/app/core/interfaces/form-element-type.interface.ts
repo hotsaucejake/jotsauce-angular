b77692a4c38bd71708f60e3d02a16ed45cb0cbc8
hotsaucejake <get@hotsaucejake.com>
2021-03-02 10:03:32 -0500

    update interfaces based on api response

--- src/app/core/interfaces/form-element-type.interface.ts
+++ src/app/core/interfaces/form-element-type.interface.ts
@@ -1,12 +1,13 @@
 import { FormElement } from './form-element.interface';
 
 export interface FormElementType {
-    id: number;
+    type_id: number;
     type: string;
     title: string;
     description: string;
     order_column: number;
     created_at?: Date;
     updated_at?: Date;
-    form_element: FormElement;
+    form_element: string;
+    form_element_id: number;
 }
